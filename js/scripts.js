9515b11608d1aef08cbd36131d83e5e55614f0e3
Jose Armando <josearmandodiazhernandez1@gmail.com>
2024-09-18 23:58:19 -0500

    Añadido carousel y marquee al index, login y register añadidos

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
 window.addEventListener('scroll', function() {
     let header = document.querySelector('header');
     if (this.window.scrollY > 50) {
@@ -6,3 +10,15 @@ window.addEventListener('scroll', function() {
         header.classList.remove('scrolled');
     }
 })
+
+//
+
+// Evento para abrir form de registro
+signUpButton.addEventListener('click', () =>
+  container.classList.add('right-panel-active')
+);
+
+// Evento para regresar al form de iniciar sesión
+signInButton.addEventListener('click', () =>
+  container.classList.remove('right-panel-active')
+);
