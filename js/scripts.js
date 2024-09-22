917cdb45b534e874ab2b8cb8faa389117c81c90b
Jose Armando <josearmandodiazhernandez1@gmail.com>
2024-09-21 23:40:02 -0500

    Responsive mejorado, semantica mejorada.

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ const botonIniciarSesion = document.getElementById('iniciarSesion');
 const contenedor = document.getElementById('contenedor');
 
 window.addEventListener('scroll', function() {
-    let header = document.querySelector('header');
+    let header = document.getElementById('header');
     if (this.window.scrollY > 50) {
         header.classList.add('scrolled');
     } else {
