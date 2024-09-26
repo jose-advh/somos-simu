26d7fb4e206eac2332c5e0e8fb6f0cf620d87e61
Jose Armando <josearmandodiazhernandez1@gmail.com>
2024-09-26 16:34:19 -0500

    Index optimizado, comunidad añadida

--- acceder.php
+++ acceder.php
@@ -1,19 +1,17 @@
 <!DOCTYPE html>
 <html lang="es">
-
-
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <script src="https://kit.fontawesome.com/869dc8f5ef.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="../css/styles.css" />
+    <link rel="stylesheet" href="./css/styles.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Simu - Acceder</title>
 </head>
 
 <body class="body-acceder">
     <header>
-        <a href="/index.html" class="header__volver">Volver</a>
+        <a href="./index.html" class="header__volver">Volver</a>
     </header>
     <div class="contenedor" id="contenedor">
 
@@ -73,7 +71,7 @@
             </div>
         </div>
     </div>
-    <script src="../js/scripts.js"></script>
+    <script src="./js/scripts.js"></script>
 <?php  
 include ("php/registro.php");
 ?>
