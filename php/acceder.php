3a0a3fd23815d5281aa293662b315c3472d2e603
imLost14 <hrdc.1407@hotmail.com>
2024-09-27 12:15:42 -0500

    faltaba ruta de estilo en acceder

--- php/acceder.php
+++ php/acceder.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <script src="https://kit.fontawesome.com/869dc8f5ef.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="./css/styles.css" />
+    <link rel="stylesheet" href="../css/styles.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Simu - Acceder</title>
 </head>
